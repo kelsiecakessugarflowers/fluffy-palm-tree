ead3278b2144a3faa5d7b3f9c61abd6019d6ea5c
kelsiecakessugarflowers <kelsie@kelsiecakes.com>
2025-11-24 12:59:18 -0500

    Render placeholder when ACF unavailable

--- kelsie-review-block.php
+++ kelsie-review-block.php
@@ -3,12 +3,12 @@
  * Plugin Name: Kelsie Review Block
  * Description: Custom testimonial block with ACF repeater + Rank Math schema.
  * Author: It Me
- * Version: 2.4.4
+ * Version: 2.4.5
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'KELSIE_REVIEW_BLOCK_VERSION', '2.4.4' );
+define( 'KELSIE_REVIEW_BLOCK_VERSION', '2.4.5' );
 
 	/* -----------------------------------------------------------
 	 *  BRAND DESIGN CONSTANTS
